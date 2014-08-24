e9ad419500848b694cadcf254a92f16b5359f6ac
Matic Jurglič <matic@jurglic.si>
2014-08-24 12:15:38 +0200

    Update twitter.rb

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -3,10 +3,10 @@ require 'twitter'
 #### Get your twitter keys & secrets:
 #### https://dev.twitter.com/docs/auth/tokens-devtwittercom
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key = 'VSfbLVRiTjsIKzJnQqxBQ'
-  config.consumer_secret = 'p7ZuIr5NKyRcCe9aKY1qU9YXQhkB1ohVweQNp91Bq0'
-  config.access_token = '60061639-s2X26FcxaoI1YwKpbD3ScTHhHSIo9yPwi5jQ0AzTF'
-  config.access_token_secret = 'GDVzq58rujvlizyzw8y0jNH0PUwM8LpZm34ZZ86wbZpOH'
+  config.consumer_key = ''
+  config.consumer_secret = ''
+  config.access_token = ''
+  config.access_token_secret = ''
 end
 
 SCHEDULER.every '2m' do
